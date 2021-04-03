b042a03566ed9ece6f17a73e0784bbd288d3155c
JhsK <qwe6293@nate.com>
2021-04-03 15:17:40 +0900

    [Feat] 경기도 data 변수 2자리로 인한 데이터 값 받는 버그 수정

--- js/openApi.js
+++ js/openApi.js
@@ -2,21 +2,18 @@ const locImg = document.querySelector("figure");
 const locTitle = document.querySelector(".test");
 
 const urlParams = window.location.href;
-console.log(urlParams.indexOf("params"));
-console.log(urlParams[67]);
+let areaCode = 0;
 
 if (urlParams.indexOf("params")) {
-  const areaCode = urlParams[67];
+  urlParams[68] ? (areaCode = urlParams.slice(67)) : (areaCode = urlParams[67]);
 
   axios
     .get(
       `http://api.visitkorea.or.kr/openapi/service/rest/GreenTourService/areaBasedList?ServiceKey=Q3CYAtHS7fxCOqd8Kns3aZJmrmuioRJoSSKZv7GeVPhoVgX8oKevWlLyBT0NGMkHaVWihIRaOwEM2v5CKVqtGA%3D%3D&areaCode=${areaCode}&MobileOS=ETC&MobileApp=Tourist&_type=json`
     )
     .then((result) => {
-      console.log(result.data.response.body.items.item[0].title);
       let apiTitle = result.data.response.body.items.item[2].title;
       let apiImgUrl = result.data.response.body.items.item[2].mainimage;
-      console.log(typeof apiTitle);
       locTitle.innerText = apiTitle;
       locImg.style.backgroundImage = `url(${apiImgUrl})`;
     })
